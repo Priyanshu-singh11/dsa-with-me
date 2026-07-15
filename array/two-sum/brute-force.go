d93c7acffd5e265f67b029508523b573a182be9d
Priyanshu-singh11 <ajitsinghsombansi1984@gmail.com>
2026-07-15 16:57:18 +0530

    Uncomment main function and import fmt

--- array/two-sum/brute-force.go
+++ array/two-sum/brute-force.go
@@ -1,19 +1,19 @@
 package main
 
+import "fmt"
+
 // import "fmt"
 
-// // import "fmt"
+func main() {
+	arr := []int{2, 7, 11, 15}
+	target := 9
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			sum := arr[i] + arr[j]
+			if sum == target {
+				fmt.Println(arr[i], arr[j])
+			}
+		}
+	}
 
-// func main() {
-// 	arr := []int{2, 7, 11, 15}
-// 	target := 9
-// 	for i := 0; i < len(arr); i++ {
-// 		for j := i + 1; j < len(arr); j++ {
-// 			sum := arr[i] + arr[j]
-// 			if sum == target {
-// 				fmt.Println(arr[i], arr[j])
-// 			}
-// 		}
-// 	}
-
-// }
+}

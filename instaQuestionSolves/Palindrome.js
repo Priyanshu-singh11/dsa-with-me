e082b53747eb28fbd87c7067c92d21e4a7a22764
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-06 12:58:33 +0530

    Implement optimized palindrome checker function
    Added a new function for optimized palindrome checking.

--- instaQuestionSolves/Palindrome.js
+++ instaQuestionSolves/Palindrome.js
@@ -1,4 +1,18 @@
 function palindromeChecker(str) {
+    
+    let left = 0;
+    let right = str.length-1;
+    let newStr = "";
+    while (right>=left){
+        newStr += str[right]
+        right--
+    }
+    return newStr===str
+}
+
+
+
+function OptimizedPalindromeChecker(str) {
     let n = str.length-1;
     let i = 0
     for(n;n>=0;n--){

e81f92a7787555115686cb182369900de32005ce
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-07 19:00:15 +0530

    Implement multiplyExceptSelf function
    Add a function to multiply elements except self.

--- instaQuestionSolves/exceptOfSelfMultiply.js
+++ instaQuestionSolves/exceptOfSelfMultiply.js
@@ -9,3 +9,24 @@ function OptimizedMultiplyExceptSelf(arr){
 
 let result=OptimizedMultiplyExceptSelf(arr)
 console.log(result)
+
+let arr = [1,4,5,6,7]
+
+
+function multiplyExceptSelf(arr){
+ let newArr = []
+ for(let i=0;i<arr.length;i++){
+  let sum = 1;
+    for(let j=0;j<arr.length;j++){
+      if(i!==j){
+       sum *= arr[j]
+       //console.log(sum,arr[j])
+      }
+    }
+    newArr.push(sum)
+}
+return newArr
+}
+
+let res=multiplyExceptSelf(arr)
+console.log(res)

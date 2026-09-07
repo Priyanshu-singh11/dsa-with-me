6804ed1ef656d356419c3ac56a7370e675f514f5
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-07 07:48:35 +0530

    Implement OptimizeCaptialize function
    Added OptimizeCaptialize function to capitalize specific words in a string.

--- instaQuestionSolves/CapitalizeWord.js
+++ instaQuestionSolves/CapitalizeWord.js
@@ -1,3 +1,4 @@
+let str = "hello this is javaScript"
 function captalize(str,index){
  let newStr = "";
  for(let i=0;i<=str.length-1;i++){
@@ -10,3 +11,23 @@ function captalize(str,index){
  return newStr
 }
 console.log(captalize(str,10))
+
+
+
+
+
+
+
+function OptimizeCaptialize(str,word){
+ let newStr = "";
+ for (let elem of str) {
+  if(elem===word){
+   newStr += elem.toUpperCase()
+  }
+  else{
+   newStr += elem
+  }
+ }
+
+ return newStr
+}

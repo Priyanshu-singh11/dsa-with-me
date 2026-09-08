28d327b9c5714dc2dcfbc538feade810a32f1885
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-08 13:30:12 +0530

    Implement matrix multiplication with validation
    Added validation to check if matrices can be multiplied.

--- OwnDSAQuestionPractice/Matrix/multiplicationMatrix.js
+++ OwnDSAQuestionPractice/Matrix/multiplicationMatrix.js
@@ -1,9 +1,15 @@
+
 function multiplyMatrix(arr1,arr2){
- 
  let tempArr = []
+ if(arr2.length!==arr1.length){
+   return "Not a valid matrix "
+  }
  for(let i=0;i<arr1.length;i++){
   let matrix = [];
   for(let j=0;j<arr1[i].length;j++){
+   if(arr2[i].length!==arr1[i].length){
+   return "Not a valid matrix "
+  }
    let add = arr1[i][j]*arr2[i][j]
     matrix.push(add)
   }

b83672228d2b7331fe21b17817e6020822bb7d0c
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-20 11:04:07 +0530

    Update Buy and sell.go

--- array/Buy and sell/Buy and sell.go
+++ array/Buy and sell/Buy and sell.go
@@ -1,1 +1,14 @@
 
+func maxProfit(prices []int) int {
+    var maxProfit,bestBuy = 0, prices[0]
+    for i:=0;i<len(prices);i++{
+        if prices[i]<bestBuy{
+           bestBuy = prices[i]
+        }
+        profit := prices[i]-bestBuy
+        if profit>maxProfit{
+            maxProfit = profit
+        }
+    }
+    return maxProfit
+}

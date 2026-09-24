953b01d68031143d46a7c692a2ab9277794e703d
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-24 16:40:27 +0530

    Refactor commonNumberArray and update arr3
    Updated arr3 to include an additional element and refactored the commonNumberArray function for improved readability and logic.

--- instaQuestionSolves/ExtractCommonNumberFromArray.js
+++ instaQuestionSolves/ExtractCommonNumberFromArray.js
@@ -1,27 +1,24 @@
 let arr1 = [4,6,89,9,0]
 let arr2 = [5,8,5,7,8]
-let arr3 = [2,4,5,6,7,]
+let arr3 = [2,4,5,6,7,8]
 
 
 function commonNumberArray(...arr){
  let newArr = [];
- let isFound = false
- for(let i=0; i<arr.length;i++){
-  for(let j=0; j<arr[i].length;j++){
-  for(let k=0;k<newArr.length;k++){
-  
-   if(newArr[k]===arr[i][j]){
-    isFound = true;
-    break
+ for(let i=0;i<arr.length;i++){
+  for(let j=0;j<arr[i].length;j++){
+   let isHave = false
+   for(let k=0;k<newArr.length;k++){
+    if(newArr[k]===arr[i][j]){
+     isHave = true
+    }
+   }
+   if(!isHave){
+    newArr.push(arr[i][j])
    }
   }
-  
-   if(!isFound){
-    //console.log(arr[i][j])
-    newArr.push(arr[i][j])
  }
- }
- }
+ 
  return newArr
 }
 let res = commonNumberArray(arr1,arr2,arr3)

335ec958111ab47d9e06dff2eb81ae4a2599a625
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-25 17:03:36 +0530

    Update SubArraySum__OptimizedApproch.js

--- array/Maximum Sub Array Sum/Kadane's Algorithm/SubArraySum__OptimizedApproch.js
+++ array/Maximum Sub Array Sum/Kadane's Algorithm/SubArraySum__OptimizedApproch.js
@@ -1,1 +1,15 @@
 
+let arr = [3,-4,5,4,-1,7,-8]
+let currElem = 0;
+let maxElem = -Infinity
+
+for(let i=0;i<arr.length;i++){
+ currElem += arr[i]
+ if(currElem>maxElem){
+  maxElem = currElem
+ }
+ if(currElem<0){
+  currElem = 0
+ }
+}
+console.log(maxElem)

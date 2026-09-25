a52c0dd6a7fbca95c52b37168c7c8d37f02e7f66
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-25 16:30:59 +0530

    Update BruteForceApproch.js

--- array/Maximum Sub Array Sum/BruteForceApproch.js
+++ array/Maximum Sub Array Sum/BruteForceApproch.js
@@ -1,1 +1,25 @@
 
+let n = [3,-4,5,4,-1,7,-8]
+let maxElem = -Infinity;
+let possibleValuesOfMax = []
+let startIdx = 0
+let endIdx = 0
+let maxSubArray = []
+for(let sta=0;sta<n.length;sta++){
+ let currElem = 0
+ let val = ""
+ for(let end=sta;end<n.length;end++){
+  currElem += n[end]
+  if(currElem>maxElem){
+   possibleValuesOfMax.push(currElem)
+   startIdx = sta
+   endIdx = end
+   maxElem = currElem
+  }
+  //maxElem = Math.max(currElem,maxElem)
+ }
+}
+maxSubArray.push(...n.slice(startIdx,endIdx+1))
+console.log(maxElem)
+console.log(possibleValuesOfMax)
+console.log(maxSubArray)

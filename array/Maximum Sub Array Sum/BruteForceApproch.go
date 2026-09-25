ca46921855f1ff80d1637171521358307e1b0364
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-25 17:30:17 +0530

    Update BruteForceApproch.go

--- array/Maximum Sub Array Sum/BruteForceApproch.go
+++ array/Maximum Sub Array Sum/BruteForceApproch.go
@@ -1,1 +1,15 @@
 
+func maxSubArray(nums []int) int {
+    var maxElem int = math.MinInt
+    
+    for i:=0;i<len(nums);i++{
+        currElem := 0
+        for j:=i;j<len(nums);j++{
+            currElem += nums[j]
+            if currElem>maxElem{
+                maxElem = currElem
+            }
+        }
+    }
+    return maxElem
+}

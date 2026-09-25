5c751359aced7cb37d435060ede4874542e9c0cf
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-25 17:17:57 +0530

    Update MaxSubArraySum_optimizedApproch.go

--- array/Maximum Sub Array Sum/Kadane's Algorithm/MaxSubArraySum_optimizedApproch.go
+++ array/Maximum Sub Array Sum/Kadane's Algorithm/MaxSubArraySum_optimizedApproch.go
@@ -1,1 +1,16 @@
 
+func maxSubArray(nums []int) int {
+    //var maxElem = math.MinInt
+    maxElem := int(math.Inf(-1))
+    currElem := 0
+    for _,num := range nums{
+        currElem += num
+        if currElem>maxElem{
+            maxElem = currElem
+        }
+        if currElem<0{
+            currElem = 0
+        }
+    }
+    return maxElem
+}

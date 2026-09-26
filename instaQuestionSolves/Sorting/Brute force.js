107e03c54f689beffe305af0c3a08a3687913a0e
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-26 14:56:37 +0530

    Update Brute force.js

--- instaQuestionSolves/Sorting/Brute force.js
+++ instaQuestionSolves/Sorting/Brute force.js
@@ -1,1 +1,10 @@
-
+let data = [6,8,18,527,69,8,1]
+let newData = []
+for(let i=0;i<data.length;i++){
+ for(let j=i+1;j<data.length;j++){
+  if(data[i]>data[j]){
+   [data[j],data[i]] = [data[i],data[j]]
+  }
+ }
+}
+console.log(data)

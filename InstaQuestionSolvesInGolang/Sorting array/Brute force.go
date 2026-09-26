0378592b51d3bc366b78f7a8ea5ee5f3c70201d1
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-26 14:59:05 +0530

    Update Brute force.go

--- InstaQuestionSolvesInGolang/Sorting array/Brute force.go
+++ InstaQuestionSolvesInGolang/Sorting array/Brute force.go
@@ -1,1 +1,10 @@
 
+data := []int{6,8,18,527,69,8,1}
+for i := 0; i < len(data); i++ {
+  for j := i+1; j < len(data); j++ {
+    if data[i] > data[j] {
+      data[i], data[j] = data[j], data[i]
+    }
+  }
+}
+fmt.Println(data)

66dc9613a852b4e1299266361d83cf16e8db6c73
PRIYANSHU SINGH <ajitsinghsombansi1984@gmail.com>
2026-09-26 15:18:59 +0530

    Update Remove duplicate String.js

--- instaQuestionSolves/Remove duplicate String.js
+++ instaQuestionSolves/Remove duplicate String.js
@@ -1,1 +1,17 @@
 
+let name = "Hello"
+let repStr = []
+for(let i=0;i<name.length;i++){
+ let isHave = false
+ for(let j=0;j<repStr.length;j++){
+  
+  if(name[i]===repStr[j]){
+   isHave = true
+   break
+  }
+ }
+ if(!isHave){
+   repStr.push(name[i])
+  }
+}
+console.log(repStr)
